9bec8b917013c9a99759bf79a72dc690ecf89a1d
rahulreddy <v.rahul1719@gmail.com>
2017-08-14 19:51:27 +0530

    Largest odd number

--- LargestOddNumber.py
+++ LargestOddNumber.py
@@ -1,12 +1,17 @@
-import sys
-
 counter = 0
-numb = [None, None, None]
+largest_odd_number = 0
+input_numbers = [None, None, None]
 while counter <= 2:
     x = input("Enter a number: ")
     # line= sys.stdin
-if type(int(x)) == int:
-    numb[counter] = x
-    counter += 1
-    numb.
-    print(x)
+    if type(int(x)) == int:
+        input_numbers[counter] = x
+        counter += 1
+        for numb in input_numbers:
+            # print(type(numb))
+            if isinstance(numb, str) and int(numb) % 2 != 0:
+                # print(numb)
+                if (int(largest_odd_number) < int(numb)):
+                    largest_odd_number = numb
+
+print("Largets odd number is " + largest_odd_number)
